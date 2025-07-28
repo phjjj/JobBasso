4782abe54db10e74ed90ee3c33d9785dbdb8efaa
박해준 <102671646+phjjj@users.noreply.github.com>
2025-07-28 14:07:24 +0900

    first-commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { styled } from '@linaria/react'
+
+export default function Home() {
+  return (
+    <StyledDiv>홈</StyledDiv>
+  );
+}
+
+const StyledDiv = styled.div`
+  color: red;
+`;
---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+        'next/babel',
+        '@babel/preset-typescript',
+        '@wyw-in-js/babel-preset'
+    ],
+};
