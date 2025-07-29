a2287f626f0970dd54dda15820a27a9de9444997
박해준 <102671646+phjjj@users.noreply.github.com>
2025-07-29 17:53:58 +0900

    fix: 린트 커밋 수정

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': [
-    'eslint --fix --quiet --max-warnings=0',
+    /** 한번에 fix하고 체크하니 오류가 발생하는 경우 커밋이 실패함 */
+    'eslint --fix --quiet',
+    'eslint --quiet --max-warnings=0',
     'prettier --write --loglevel=error',
   ],
   '*.{json,css,md}': ['prettier --write --loglevel=error'],
