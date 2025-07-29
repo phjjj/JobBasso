9bcaa92b481eda7ef00559a326cc9f1374ecefe6
박해준 <102671646+phjjj@users.noreply.github.com>
2025-07-29 10:53:11 +0900

    fix : 린트 관련 수정

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-    webpack(config, { dev, isServer }) {
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            exclude: /node_modules/,
-            use: [
-                {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            'next/babel',
-                            '@babel/preset-typescript',
-                            '@wyw-in-js/babel-preset'
-                        ],
-                    },
-                },
-                {
-                    loader: '@wyw-in-js/webpack-loader',
-                    options: {
-                        sourceMap: dev,
-                    },
-                },
-            ],
-        });
- 
-        return config;
-    },
+  /* config options here */
 };
- 
+
 export default nextConfig;
